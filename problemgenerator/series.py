8020a42ced4ed12fd405af5795cc176d39d3d5b0
Juha Myllari <jmyllari@gmail.com>
2019-05-16 20:27:16 +0300

    add TupleSeries class

--- problemgenerator/series.py
+++ problemgenerator/series.py
@@ -8,4 +8,16 @@ class Series:
   def process(self, data):
     data_length = data.shape[0]
     return np.array([self.child.process(data[i,...]) for i in range(data_length)])
-  
+
+
+class TupleSeries:
+
+  def __init__(self, children):
+    self.children = children
+
+  def process(self, data):
+    data_length = data[0].shape[0]
+    as_list = [np.array(
+      [child.process(data[index][j,...]) for j in range(data_length)])
+      for (index, child) in enumerate(self.children)]
+    return tuple(as_list)

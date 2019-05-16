69d5bc0655975437518d2eac28fd2831e585b113
Juha Myllari <jmyllari@gmail.com>
2019-05-16 18:50:45 +0300

    improve legibility

--- errgen.py
+++ errgen.py
@@ -1,6 +1,6 @@
 import sys
 import numpy as np
-import problemgenerator.tensor as tensor
+import problemgenerator.array as array
 import problemgenerator.filter as filter
 import problemgenerator.series as series
 
@@ -14,26 +14,27 @@ import problemgenerator.series as series
 # (each data point corresponding to, say, a different day)
 observations, sensors = 100, 10
 
-# Create fake (random) data
+# Create a matrix of (random) data to use as input
 data = np.random.randn(observations, sensors)
 
-# Create a Tensor to represent the battery of 10 sensors
-t = tensor.Tensor(sensors)
+# Create an Array object to represent the battery of 10 sensors
+sensor_array = array.Array(sensors)
 
 # Add a Missing filter to randomly transform elements to Nan
 # (NaN = "not a number", i.e. missing or invalid data)
-t.addfilter(filter.Missing(probability=.3))
+sensor_array.addfilter(filter.Missing(probability=.3))
 
 # Create a series to represent the 100 data points
-s = series.Series(t)
+observation_series = series.Series(sensor_array)
 
-# Process the data
-out = s.process(data)
+# The data model tree is now complete.
+# Process the data to introduce errors
+output = observation_series.process(data)
 
 # Sanity check: does the shape of the output equal that of the input?
 print("input data has shape", data.shape)
-print("output data has shape", out.shape)
+print("output data has shape", output.shape)
 
 # The relative frequency on NaNs should be close to the probability
 # given as a parameter to the Missing filter
-print("relative frequency of NaNs:", np.isnan(out).sum() / out.size)
+print("relative frequency of NaNs:", np.isnan(output).sum() / output.size)

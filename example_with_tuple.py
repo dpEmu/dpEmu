19b13282bf7e0ec189a6c1fff5883595dad80591
Juuso Silvennoinen <32332926+Jsos17@users.noreply.github.com>
2019-05-17 03:25:35 +0300

    Auto-format code

--- example_with_tuple.py
+++ example_with_tuple.py
@@ -28,10 +28,10 @@ output = root.process(data)
 print("Output type (should be tuple):", type(output))
 print("Output length (should be 2):", len(output))
 print("Shape of first member of output tuple (should be (100, 10)):",
-  output[0].shape)
+      output[0].shape)
 print("Shape of second first member of output tuple (should be (100,)):",
-  output[1].shape)
+      output[1].shape)
 print("Number of NaNs in x (should be 0):",
-  np.isnan(output[0]).sum())
+      np.isnan(output[0]).sum())
 print(f"Number of NaNs in y (should be close to {probability * y.size}):",
-  np.isnan(output[1]).sum())
+      np.isnan(output[1]).sum())

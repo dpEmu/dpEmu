05f1eb24215dfdbad40f89189b90345694db1822
Juha Myllari <jmyllari@gmail.com>
2019-05-19 20:37:47 +0300

    variable naming

--- example_gaussian_noise.py
+++ example_gaussian_noise.py
@@ -4,7 +4,7 @@ import problemgenerator.filter as filter
 import problemgenerator.series as series
 import matplotlib.pyplot as plt
 
-x_file, y_file = "example_data/x.npy", "example_data/y.npy"
+x_file, y_file = "example_data/mnist_subset/x.npy", "example_data/mnist_subset/y.npy"
 x = np.load(x_file)
 y = np.load(y_file)
 data = (x, y)
@@ -17,10 +17,13 @@ root_node = series.TupleSeries([x_node, y_node])
 out_x, out_y = root_node.process(data)
 print((y != out_y).sum(), "elements of y have been modified in (should be 0).")
 
-fig, axs = plt.subplots(2, 4)
-for i in range(4):
+examples = 4
+fig, axs = plt.subplots(2, examples)
+for i in range(examples):
     img_ind = np.random.randint(len(x))
     axs[0, i].matshow(x[img_ind], cmap='gray_r')
+    axs[0, i].axis('off')
     axs[1, i].matshow(out_x[img_ind], cmap='gray_r')
+    axs[1, i].axis('off')
 
 plt.show()

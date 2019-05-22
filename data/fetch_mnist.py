834b84dcfd9f9e8711ecd252b0480267a5294034
Juha Harviainen <juha.harviainen@gmail.com>
2019-05-22 16:24:41 +0300

    Make fetch_mnist save data in numpy's own format

--- data/fetch_mnist.py
+++ data/fetch_mnist.py
@@ -1,29 +1,20 @@
-from sklearn.datasets import fetch_openml
-import json
 import os.path
 
-data_filename = "mnist_data.json"
-label_filename = "mnist_label.json"
+from sklearn.datasets import fetch_openml
+
+data_filename = "mnist_data.txt"
+label_filename = "mnist_label.txt"
 
 def fetch_mnist():
     print("Fetching MNIST data")
     mnist = fetch_openml('mnist_784')
     data = mnist['data']
     target = mnist['target'].astype(int)
-    
-    data_obj = {"data": data.tolist()}
-    label_obj = {"labels": target.tolist()}
 
     print("Saving data to " + data_filename + " and " + label_filename)
 
-    data_file = open(data_filename, 'w')
-    label_file = open(label_filename, 'w')
-
-    data_file.write(json.dumps(data_obj))
-    label_file.write(json.dumps(label_obj))
-
-    data_file.close()
-    label_file.close()
+    data.tofile(data_filename)
+    target.tofile(label_filename)
 
 if __name__ == "__main__":
     if os.path.isfile(data_filename) and os.path.isfile(label_filename):

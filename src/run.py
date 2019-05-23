d320e7d2a001e51645b4df864f4eed5cff6f0583
Juha Mylläri <jmyllari@gmail.com>
2019-05-23 11:59:53 +0300

    load data

--- src/run.py
+++ src/run.py
@@ -1,5 +1,9 @@
 import sys
 import numpy as np
+import src.problemgenerator.series as series
 
-original_data_files = sys.argv[1:]
+original_data_files = sys.argv[2:]
 
+original_data = [np.load(data_file) for data_file in original_data_files]
+
+error_generator_root = series.TupleSeries(original_data)

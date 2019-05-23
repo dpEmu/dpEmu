1860b4fb3d86dcad6721cc8287d707425c1b9cbb
Juha Mylläri <jmyllari@gmail.com>
2019-05-23 13:01:20 +0300

    produce save file names

--- src/run.py
+++ src/run.py
@@ -1,14 +1,20 @@
 import sys
+import datetime
 import numpy as np
 import problemgenerator.series as series
 import problemgenerator.array as array
 import problemgenerator.filter as filter
 
+
+def create_datestamped_filename(prefix, extension):
+    timestamp_string = str(datetime.datetime.utcnow().timestamp())
+    return f"{prefix}_{timestamp_string}.{extension}"
+
 # To be read from file (file name given as argument)!
 std, prob_missing = .1, .2
 
 # To be taken as arguments
-original_data_files = ["../data/mnist_data.npy", "../data/mnist_label.npy"]
+original_data_files = ["../data/mnist_subset/x.npy", "../data/mnist_subset/y.npy"]
 
 original_data = tuple([np.load(data_file) for data_file in original_data_files])
 
@@ -19,4 +25,6 @@ y_node = array.Array(original_data[1][0].shape)
 error_generator_root = series.TupleSeries([x_node, y_node])
 
 errorified_data = error_generator_root.process(original_data)
-#print(errorified_data[0].shape, errorified_data[1].shape)
+# print(errorified_data[0].shape, errorified_data[1].shape)
+# print(create_datestamped_filename("x", "npy"))
+# print(create_datestamped_filename("y", "npy"))

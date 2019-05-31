0ea1ef422eba98400a574bcc495824d42d440faa
Antti Roeyskoe <aroysko@paivola.fi>
2019-05-31 16:10:46 +0300

    change 'this' to 'self' in param_selector.py

--- src/paramselector/param_selector.py
+++ src/paramselector/param_selector.py
@@ -1,8 +1,8 @@
 # Example parameter selector. Just outputs given commands in order
 class ParamSelector:
-    def __init__(this, parsel_config_filename=None, model_config_filename=None, error_config_filename=None):
-        this.commands_list = []
-        this.commands_ind = 0
+    def __init__(self, parsel_config_filename=None, model_config_filename=None, error_config_filename=None):
+        self.commands_list = []
+        self.commands_ind = 0
         if parsel_config_filename is not None:
             f = open(parsel_config_filename)
             while True:
@@ -10,15 +10,15 @@ class ParamSelector:
                 run_analyze_command = f.readline().rstrip('\n')
                 if not run_analyze_command:
                     break
-                this.commands_list.append([run_model_command, run_analyze_command])
+                self.commands_list.append([run_model_command, run_analyze_command])
 
-    def should_continue(this):
-        return this.commands_ind < len(this.commands_list)
+    def should_continue(self):
+        return self.commands_ind < len(self.commands_list)
 
-    def next_commands(this):
-        i = this.commands_ind
-        this.commands_ind += 1
-        return this.commands_list[i][0], this.commands_list[i][1]
+    def next_commands(self):
+        i = self.commands_ind
+        self.commands_ind += 1
+        return self.commands_list[i][0], self.commands_list[i][1]
 
-    def read_analysis(this, analysis):
+    def read_analysis(self, analysis):
         pass

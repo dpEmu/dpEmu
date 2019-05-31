7d8e3c0ba16f4c591fd4a17baca35c4923cb72e2
Juuso Silvennoinen <32332926+Jsos17@users.noreply.github.com>
2019-06-01 01:44:25 +0300

    Add ocr-error filter

--- run_string_example.py
+++ run_string_example.py
@@ -7,7 +7,10 @@ data = np.array(["hello world",
                  "all your Bayes' theorems are belong to us"])
 
 x_node = array.Array(data.shape)
-x_node.addfilter(filters.Uppercase(.45))
+# x_node.addfilter(filters.Uppercase(.45))
+
+replacements = {"e": (["E", "i"], [.5, .5]), "g": (["q", "9"], [.2, .8])}
+x_node.addfilter(filters.OCRerror(0, replacements))
 root_node = copy.Copy(x_node)
 out = root_node.process(data)
 print(out)
--- src/problemgenerator/filters.py
+++ src/problemgenerator/filters.py
@@ -1,3 +1,5 @@
+import random
+from copy import deepcopy
 import numpy as np
 
 
@@ -32,6 +34,7 @@ class GaussianNoise(Filter):
                                               scale=self.std,
                                               size=data[index_tuple].shape)
 
+
 class Uppercase(Filter):
 
     def __init__(self, probability):
@@ -47,5 +50,34 @@ class Uppercase(Filter):
 
         for index, element in np.ndenumerate(data[index_tuple]):
             original_string = element
-            modified_string = "".join([stochastic_upper(c, self.prob) for c in original_string])
+            modified_string = "".join(
+                [stochastic_upper(c, self.prob) for c in original_string])
             data[index_tuple][index] = modified_string
+
+
+class OCRerror(Filter):
+
+    def __init__(self, p, replacements):
+        """ Pass replacements as a dict.
+
+        For example {"e": (["E", "i"], [.5, .5]), "g": (["q", "9"], [.2, .8])}
+        where the latter list consists of probabilities which sum to 1"""
+
+        self.prob = p
+        self.replacements = replacements
+        super().__init__()
+
+    def apply(self, data, index_tuple):
+        if np.random.random_sample() > self.prob:
+            for index, string_ in np.ndenumerate(data[index_tuple]):
+                data[index_tuple][index] = self.generate_ocr_errors(string_)
+
+    def generate_ocr_errors(self, string_):
+        return "".join([self.__replace_char(c, self.replacements) for c in string_])
+
+    def __replace_char(self, c, replacements):
+        if c in replacements:
+            chars, probs = replacements[c]
+            return random.choices(chars, probs)[0]
+
+        return c

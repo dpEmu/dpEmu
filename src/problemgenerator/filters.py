b6b98e8c9b558cb2de3770b008215adbd10928bc
Juuso Silvennoinen <32332926+Jsos17@users.noreply.github.com>
2019-06-01 21:56:04 +0300

    Add seed for Python's own RNG

--- src/problemgenerator/filters.py
+++ src/problemgenerator/filters.py
@@ -62,6 +62,7 @@ class OCRerror(Filter):
         For example {"e": (["E", "i"], [.5, .5]), "g": (["q", "9"], [.2, .8])}
         where the latter list consists of probabilities which should sum to 1."""
 
+        random.seed(42)
         self.prob = p
         self.replacements = replacements
         super().__init__()

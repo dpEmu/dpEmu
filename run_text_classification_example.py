4b9c9e710c487d21062b542d2b4d74a544ea0415
thalvari <tuomas.halvari@helsinki.fi>
2019-06-03 06:10:11 +0300

    revert back to using pipeline

--- run_text_classification_example.py
+++ run_text_classification_example.py
@@ -16,43 +16,42 @@ def main():
     path_to_data, path_to_labels, path_to_label_names = load_newsgroups_as_pickle(categories)
     # path_to_data, path_to_labels, path_to_label_names = load_newsgroups_as_pickle()
     path_to_clf_param_grid = generate_unique_path("tmp", "json")
-    path_to_vectorized_data = generate_unique_path("tmp", "npz")
     path_to_fitted_clf = generate_unique_path("tmp", "joblib")
-    path_to_scores = generate_unique_path("tmp", "json")
     path_to_best_clf_params = generate_unique_path("tmp", "json")
+    path_to_scores = generate_unique_path("tmp", "json")
     path_to_confusion_matrix = generate_unique_path("tmp", "npy")
 
     clf_param_grid = {
-        "alpha": [10 ** i for i in range(-3, 1)],
-        # "C": [10 ** k for k in range(-3, 4)],
+        "multinomial_nb__alpha": [10 ** i for i in range(-3, 1)],
+        # "truncated_svd__n_components": [100, 150, 200, 250, 300],
+        # "linear_svc__C": [10 ** k for k in range(-3, 4)],
     }
     with open(path_to_clf_param_grid, "w") as file:
         json.dump(clf_param_grid, file)
 
-    run_ml_script("python src/ml/multinomialnb_model.py {} {} {} {} {}".format(
+    run_ml_script("python src/ml/multinomial_nb_model.py {} {} {} {}".format(
         path_to_data,
         path_to_labels,
         path_to_clf_param_grid,
-        path_to_vectorized_data,
         path_to_fitted_clf
     ))
     run_ml_script("python src/ml/classification_analyzer.py {} {} {} {} {} {}".format(
-        path_to_vectorized_data,
+        path_to_data,
         path_to_labels,
         path_to_fitted_clf,
-        path_to_scores,
         path_to_best_clf_params,
+        path_to_scores,
         path_to_confusion_matrix
     ))
 
-    with open(path_to_scores, "r") as file:
-        scores = json.load(file)
     with open(path_to_best_clf_params, "r") as file:
         best_clf_params = json.load(file)
+    with open(path_to_scores, "r") as file:
+        scores = json.load(file)
     cm = np.load(path_to_confusion_matrix)
 
-    print(scores)
     print(best_clf_params)
+    print(scores)
     print(cm)
 
 
--- tests/ml/test_multinomial_nb.py
+++ tests/ml/test_multinomial_nb.py
@@ -15,48 +15,46 @@ def test_naive_bayes_with_analysis():
     ]
     path_to_data, path_to_labels, _ = load_newsgroups_as_pickle(categories)
     path_to_clf_param_grid = generate_unique_path("tmp", "json")
-    path_to_vectorized_data = generate_unique_path("tmp", "npz")
     path_to_fitted_clf = generate_unique_path("tmp", "joblib")
-    path_to_scores = generate_unique_path("tmp", "json")
     path_to_best_clf_params = generate_unique_path("tmp", "json")
+    path_to_scores = generate_unique_path("tmp", "json")
     path_to_confusion_matrix = generate_unique_path("tmp", "npy")
 
     clf_param_grid = {
-        "alpha": [10 ** i for i in range(-3, 1)],
+        "multinomial_nb__alpha": [10 ** i for i in range(-3, 1)],
     }
     with open(path_to_clf_param_grid, "w") as file:
         json.dump(clf_param_grid, file)
 
-    run_ml_script("python src/ml/multinomialnb_model.py {} {} {} {} {}".format(
+    run_ml_script("python src/ml/multinomial_nb_model.py {} {} {} {}".format(
         path_to_data,
         path_to_labels,
         path_to_clf_param_grid,
-        path_to_vectorized_data,
         path_to_fitted_clf
     ))
     run_ml_script("python src/ml/classification_analyzer.py {} {} {} {} {} {}".format(
-        path_to_vectorized_data,
+        path_to_data,
         path_to_labels,
         path_to_fitted_clf,
-        path_to_scores,
         path_to_best_clf_params,
+        path_to_scores,
         path_to_confusion_matrix
     ))
 
-    with open(path_to_scores, "r") as file:
-        scores = json.load(file)
     with open(path_to_best_clf_params, "r") as file:
         best_clf_params = json.load(file)
+    with open(path_to_scores, "r") as file:
+        scores = json.load(file)
     cm = np.load(path_to_confusion_matrix)
 
-    assert scores == {"train_data_mean_accuracy": 0.971, "test_data_mean_accuracy": 0.836}
-    assert best_clf_params == {"alpha": 0.01}
+    assert best_clf_params == {"multinomial_nb__alpha": 0.01}
+    assert scores == {"train_data_mean_accuracy": 0.97, "test_data_mean_accuracy": 0.823}
 
     predicted_cm = np.array([
-        [116, 2, 12, 25],
-        [2, 182, 10, 1],
-        [9, 9, 179, 4],
-        [23, 4, 10, 90]
+        [112, 2, 12, 29],
+        [1, 182, 11, 1],
+        [9, 11, 177, 4],
+        [27, 4, 9, 87]
     ])
 
     assert np.array_equal(cm, predicted_cm)

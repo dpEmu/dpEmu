234c5a20c43f13f9e54585f96d554450a9f2b3fc
thalvari <tuomas.halvari@helsinki.fi>
2019-06-03 13:59:00 +0300

    change analyzer parameters

--- src/ml/classification_analyzer.py
+++ src/ml/classification_analyzer.py
@@ -14,10 +14,12 @@ class ClassificationAnalyzer:
         with open(paths[0], "rb") as file:
             self.data = pickle.load(file)
         self.labels = np.load(paths[1])
-        self.fitted_clf = load(paths[2])
-        self.path_to_best_clf_params = paths[3]
-        self.path_to_scores = paths[4]
-        self.path_to_confusion_matrix = paths[5]
+        with open(paths[2], "rb") as file:
+            self.label_names = pickle.load(file)
+        self.fitted_clf = load(paths[3])
+        self.path_to_best_clf_params = paths[4]
+        self.path_to_scores = paths[5]
+        self.path_to_confusion_matrix_img = paths[6]
         np.random.seed(42)
 
     def analyze(self):
@@ -32,17 +34,13 @@ class ClassificationAnalyzer:
         scores = {k: round(v, 3) for k, v in scores.items()}
         cm = confusion_matrix(test_labels, predicted_test_labels)
 
+        # TODO
+
         with open(self.path_to_best_clf_params, "w") as fp:
             json.dump(self.fitted_clf.best_params_, fp)
         with open(self.path_to_scores, "w") as fp:
             json.dump(scores, fp)
-        np.save(self.path_to_confusion_matrix, cm)
-
-
-def main(argv):
-    analyzer = ClassificationAnalyzer(argv[1:])
-    analyzer.analyze()
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    ClassificationAnalyzer(sys.argv[1:]).analyze()

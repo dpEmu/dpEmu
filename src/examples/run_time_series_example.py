7a4cabe67d24128e7e30cd84c4fcb4b80edf485d
thalvari <tuomas.halvari@helsinki.fi>
2019-06-07 03:25:08 +0300

    fix time series example

--- src/examples/run_time_series_example.py
+++ src/examples/run_time_series_example.py
@@ -16,31 +16,36 @@ from keras.preprocessing.sequence import TimeseriesGenerator
 
 class Model:
     def __init__(self, data):
-        os.environ['PYTHONHASHSEED'] = str(42)
+        os.environ["PYTHONHASHSEED"] = str(42)
         random.seed(42)
         np.random.seed(42)
         tf.set_random_seed(42)
         self.data = data
 
     def run(self):
-        n_input, n_features = 3, 1
-        train_size = int(len(self.data) * .67)
-        train, test = self.data[:train_size], self.data[train_size - n_input:]
-        train_gen = TimeseriesGenerator(train, train, length=n_input)
-        test_gen = TimeseriesGenerator(test, test, length=n_input)
+        n_features = 1
+        n_input = int(len(self.data) * .2)
+        n_train = int(len(self.data) * .67)
+        n_test = len(self.data) - n_train
+        train = self.data[:n_train]
 
+        train_gen = TimeseriesGenerator(train, train, length=n_input)
         model = Sequential()
         model.add(LSTM(200, activation="relu", input_shape=(n_input, n_features)))
         model.add(Dense(1))
         model.compile(loss="mse", optimizer="adam")
+        model.fit_generator(train_gen, steps_per_epoch=1, epochs=200, verbose=0)
 
-        model.fit_generator(train_gen, epochs=200, verbose=0)
-        predicted_test = model.predict_generator(test_gen)
-        print(predicted_test)
-        predicted_test = np.concatenate((self.data[:train_size, ], predicted_test))
+        predicted_test = train
+        x_cur = train[-n_input:].reshape((1, n_input, n_features))
+        for _ in range(n_test):
+            y_cur = model.predict(x_cur)
+            predicted_test = np.concatenate([predicted_test, y_cur], axis=0)
+            x_cur = np.concatenate([x_cur, y_cur.reshape((1, 1, n_features))], axis=1)
 
-        plt.plot(self.data)
-        plt.plot(predicted_test)
+        plt.plot(self.data, label="data")
+        plt.plot(predicted_test, label="pred")
+        plt.legend()
         plt.tight_layout()
         buf = BytesIO()
         plt.savefig(buf, format="png")
@@ -54,7 +59,7 @@ class Model:
 
 
 def main():
-    data = pd.read_csv("data/temperature.csv", header=0, usecols=["Vancouver"])[1:91].values
+    data = pd.read_csv("data/passengers.csv", header=0, usecols=["passengers"]).values
     model = Model(data)
     out = model.run()
     out["prediction_img"].show()

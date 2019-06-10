9300441599e24874298be0c552766d1936262db7
Antti Roeyskoe <aroysko@paivola.fi>
2019-06-10 15:03:32 +0300

    write runner.py

---
+++ src/examples/run_runner_example.py
@@ -0,0 +1,64 @@
+from copy import deepcopy
+import numpy as np
+from src import runner
+
+# Model: ax+b = y, find a, minimize \sum |ax+b - y|
+class Model:
+    def __init__(self, b):
+        self.b = b
+
+    def run(self, data, model_params):
+        a = model_params["a"]
+        b = self.b
+        err = 0
+        for (x, y) in data:
+            err += abs(a*x+b - y)
+        return {"error" : err}
+
+# Add gaussian noise with parameters to data
+class ErrGen:
+    def __init__(self, data, seed):
+        self.data = data
+        self.seed = seed
+
+    def generate_error(self, params):
+        res = deepcopy(self.data)
+        np.random.seed(self.seed)
+        for (x, _) in res:
+            x += np.random.normal(loc=params["mean"], scale=params["std"])
+        return res
+
+# Ternary searches best parameter
+class ParamSelector:
+    def __init__(self, low, high, eps, params):
+        self.low = low
+        self.high = high
+        self.eps = eps
+        self.params = params
+    def next(self):
+        if self.high-self.low <= self.eps:
+            return None
+        left_mid = (2*self.low + self.high) / 3
+        right_mid = (self.low + 2*self.high) / 3
+        return [(self.params, {"a" : a}) for a in [left_mid, right_mid]]
+    def analyze(self, res):
+        left_mid = (2*self.low + self.high) / 3
+        right_mid = (self.low + 2*self.high) / 3
+        if res[0]["error"] <= res[1]["error"]:
+            self.high = right_mid
+        else:
+            self.low = left_mid
+
+# Example usage
+def main():
+    data = [(3, 6), (0, 1), (5, 9), (10, 22)]
+
+    model = Model(1)
+    err_gen = ErrGen(data, 0)
+    param_selector = ParamSelector(0, 10, 0.001, {"mean":0, "std":1})
+
+    res = runner.run(model, err_gen, param_selector)
+    print(res)
+
+# Call main
+main()
--- src/runner.py
+++ src/runner.py
@@ -1,47 +1,50 @@
-from multiprocessing import Process
+from threading import Thread
 from copy import deepcopy
-from inspect import getfullargspec
 import pandas as pd
 
-def runner(data, conf, errgen, param_chooser, model):
+def run(model, errgen, param_chooser):
     ''' Runs the model in parallel while parameters for error generation are provided.
-        data: original data
-        conf: constant global config for error generation, param_chooser and model
-        errgen: function for generating erronous data. Should have signature errgen(data, param, conf)
+        errgen: class for generating erronous data. Should have function
+            .generate_error(err_param), that adds error to the data according to the parameters.
         param_chooser: class for selecting parameters for error generation and the model.
-            param_chooser.next(conf) should return an array of pairs (err_param, mod_param), where
+            .next() should return an array of pairs (err_param, mod_param), where
             err_param is the parameters for error generation, and mod_param is the parameters for the model.
+            .analyze(outputs) is given an array of the outputs of the runs of the model with the last parameters.
+            this function can modify param_chooser's inner state so better next parameters can be selected
         model: class for the ML model. Can be pretrained or be trained when .run is called. Should have function
-            model.run(data, conf) or model.run(data, conf, model_params=...) where data is the data the model is run on,
-            conf is the config given to this function, and model_params is the parameters returned by param_chooser.
-            it should return an dictionary describing the results in some way.
+            .run(data, model_params=...) where data is the data the model is run on,
+            and model_params is the parameters returned by param_chooser. it should return an dictionary
+            describing the results in some way.
     '''
-    def run_model(mod, out, i, md, mp, c):
-        spec = getfullargspec(mod.run)
-        if "model_params" in spec.args:
-            out[i] = mod.run(md, c, model_params=mp)
-        else:
-            out[i] = mod.run(md, c)
+    def run_model(mod, out, i, md, mp):
+        out[i] = mod.run(md, model_params=mp)
 
     rows = []
     batches = 0
     while(True):
         batches += 1
-        params = param_chooser.next(conf)
+        params = param_chooser.next()
         if not params:
             break
+
         processes = []
-        outputs = []
+        outputs = [None] * len(params)
+
         for i, param_pair in enumerate(params):
             (err_param, mod_param) = param_pair
-            mod_data = errgen(data, err_param, conf)
-            p = Process(target=run_model,
-                        args=(deepcopy(model), outputs, i, mod_data, mod_param, deepcopy(conf)))
+            mod_data = errgen.generate_error(err_param)
+            p = Thread(target=run_model,
+                       args=(deepcopy(model), outputs, i, mod_data, mod_param))
             p.start()
             processes.append(p)
+
         for i, p in enumerate(processes):
             p.join()
+
+        param_chooser.analyze(outputs)
+
+        for i, p in enumerate(processes):
             (outputs[i]["err_param"], outputs[i]["mod_param"]) = params[i]
             outputs[i]["batch"] = batches
             rows.append(outputs[i])
-    return pd.dataframe(rows)
+    return pd.DataFrame(rows)

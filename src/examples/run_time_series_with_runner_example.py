83b9d16b79d21f946469c27313bf65952e283ad7
Antti Roeyskoe <aroysko@paivola.fi>
2019-06-10 15:50:11 +0300

    write param selector for example

--- src/examples/run_time_series_with_runner_example.py
+++ src/examples/run_time_series_with_runner_example.py
@@ -118,27 +118,14 @@ class ErrGen:
 
 # Ternary searches best parameter
 class ParamSelector:
-    def __init__(self, low, high, eps, params):
-        self.low = low
-        self.high = high
-        self.eps = eps
+    def __init__(self, params):
         self.params = params
 
     def next(self):
-        if self.high - self.low <= self.eps:
-            return None
-        left_mid = (2 * self.low + self.high) / 3
-        right_mid = (self.low + 2 * self.high) / 3
-        return [(self.params, {"a": a}) for a in [left_mid, right_mid]]
+        return self.params
 
     def analyze(self, res):
-        left_mid = (2 * self.low + self.high) / 3
-        right_mid = (self.low + 2 * self.high) / 3
-        if res[0]["error"] <= res[1]["error"]:
-            self.high = right_mid
-        else:
-            self.low = left_mid
-
+        self.params = None
 
 # Example usage
 def main():
@@ -149,7 +136,7 @@ def main():
     output = err_gen.generate_error({"mean": 5, "std": 15})
 
     model = Model(output)
-    param_selector = ParamSelector(0, 10, 0.001, {"mean": 0, "std": 1})
+    param_selector = ParamSelector([({"mean": a, "std": b}, None) for (a, b) in [(0, 0), (5, 15), (10, 20)]])
 
     res = runner.run(model, err_gen, param_selector)
     print(res)

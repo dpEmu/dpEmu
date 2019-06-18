b7cd69105f68c1d8e3d6da56e9709373bc4f2959
Antti Roeyskoe <aroysko@paivola.fi>
2019-06-18 14:30:44 +0300

    write resolution decreasing filter

---
+++ src/examples/run_resolution_example.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from PIL import Image
+
+import src.problemgenerator.array as array
+import src.problemgenerator.filters as filters
+import src.problemgenerator.copy as copy
+
+
+def main():
+    img = Image.open("demo/landscape.png")
+    data = np.array(img)
+    x_node = array.Array(data.shape)
+    x_node.addfilter(filters.Resolution(10))
+    root_node = copy.Copy(x_node)
+    result = root_node.process(data, np.random.RandomState(seed=42))
+    filtered_img = Image.fromarray(result.astype('uint8'), 'RGB')
+    filtered_img.show()
+
+
+if __name__ == "__main__":
+    main()

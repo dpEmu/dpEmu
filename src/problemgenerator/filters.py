9502c81eda9cec4a4f3a3bae4f2a78bd50702fd8
Antti Roeyskoe <aroysko@paivola.fi>
2019-06-18 14:19:14 +0300

    fix flake8 errors in new filters

--- src/problemgenerator/filters.py
+++ src/problemgenerator/filters.py
@@ -402,6 +402,7 @@ class Blur(Filter):
                             pixel_count += 1
                     data[y0][x0] = pixel_sum // pixel_count
 
+
 class Rotation(Filter):
     def __init__(self, angle):
         super().__init__()
@@ -422,6 +423,7 @@ class Rotation(Filter):
         y0 = round((resized_height - height)/2)
         data[index_tuple] = resized[y0:y0+height, x0:x0+width]
 
+
 class Brightness(Filter):
     """
     Increases or decreases brightness in the image.
@@ -436,21 +438,21 @@ class Brightness(Filter):
     def apply(self, data, random_state, index_tuple, named_dims):
         width = data[index_tuple].shape[1]
         height = data[index_tuple].shape[0]
-        original = np.copy(data)
         for y0 in range(height):
             for x0 in range(width):
                 r = float(data[y0, x0, 0]) * (1 / 255)
                 g = float(data[y0, x0, 1]) * (1 / 255)
                 b = float(data[y0, x0, 2]) * (1 / 255)
-                (h, l, s) = rgb_to_hls(r, g, b)
+                (hu, li, sa) = rgb_to_hls(r, g, b)
 
                 mult = 1 - exp(-2 * self.rat)
-                l = l * (1 - mult) + self.tar * mult
+                li = li * (1 - mult) + self.tar * mult
 
-                (r, g, b) = hls_to_rgb(h, l, s)
+                (r, g, b) = hls_to_rgb(hu, li, sa)
                 data[y0, x0, 0] = 255 * r
                 data[y0, x0, 1] = 255 * g
                 data[y0, x0, 2] = 255 * b
+
 
 class Saturation(Filter):
     """
@@ -466,21 +468,21 @@ class Saturation(Filter):
     def apply(self, data, random_state, index_tuple, named_dims):
         width = data[index_tuple].shape[1]
         height = data[index_tuple].shape[0]
-        original = np.copy(data)
         for y0 in range(height):
             for x0 in range(width):
                 r = float(data[y0, x0, 0]) * (1 / 255)
                 g = float(data[y0, x0, 1]) * (1 / 255)
                 b = float(data[y0, x0, 2]) * (1 / 255)
-                (h, l, s) = rgb_to_hls(r, g, b)
+                (hu, li, sa) = rgb_to_hls(r, g, b)
 
-                mult = 1 - exp(-2 * self.rat * s)
-                s = s * (1 - mult) + self.tar * mult
+                mult = 1 - exp(-2 * self.rat * sa)
+                sa = sa * (1 - mult) + self.tar * mult
 
-                (r, g, b) = hls_to_rgb(h, l, s)
+                (r, g, b) = hls_to_rgb(hu, li, sa)
                 data[y0, x0, 0] = 255 * r
                 data[y0, x0, 1] = 255 * g
                 data[y0, x0, 2] = 255 * b
+
 
 class LensFlare(Filter):
     def __init__(self):

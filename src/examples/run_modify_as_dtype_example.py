39307b4806d5e8340a3e16702c64504bd4bd5586
Juha Harviainen <juha.harviainen@gmail.com>
2019-06-24 14:49:15 +0300

    Add modify as dtype filter and example

---
+++ src/examples/run_modify_as_dtype_example.py
@@ -0,0 +1,45 @@
+import cv2
+
+import numpy as np
+
+import src.problemgenerator.array as array
+import src.problemgenerator.filters as filters
+import src.problemgenerator.copy as copy
+
+
+def img_to_pixel_data(img):
+    pixels = img.load()
+    data = np.zeros((img.size[1], img.size[0]), dtype=(int, 3))
+    for y in range(img.size[1]):
+        for x in range(img.size[0]):
+            data[y][x] = pixels[x, y]
+    return data
+
+
+def rotate(deg):
+    return filters.ModifyAsDataType(np.uint8, filters.Rotation(deg))
+
+
+def main():
+    data = cv2.imread("demo/landscape.png")
+    x_node = array.Array(data.shape)
+
+    # Some filters, e.g. Rotation, expect the data to have a specific data type, e.g. uint8.
+    #
+    # This example takes an image, sums the original image and 180 degrees rotated version,
+    # and then takes the average of each pixel's value.
+    #
+    # cv2's rotation requires data to be uint8, but summing them needs datatype with larger
+    # precision, and thus type conversions are required.
+    const = filters.Constant(2)
+    avg = filters.Division(filters.Addition(rotate(0.0), rotate(180.0)), const)
+    x_node.addfilter(filters.ModifyAsDataType(np.int16, avg))
+
+    root_node = copy.Copy(x_node)
+    result = root_node.process(data, np.random.RandomState(seed=42))
+    cv2.imshow("Rotated", result)
+    cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    main()

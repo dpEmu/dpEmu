ecb2cbcffcafe33a47fac08223e0c49ed9c496a1
Juha Harviainen <juha.harviainen@gmail.com>
2019-07-15 16:03:45 +0300

    Fix run_string_example

--- src/examples/run_string_example.py
+++ src/examples/run_string_example.py
@@ -1,14 +1,13 @@
 import numpy as np
+
 import src.problemgenerator.array as array
 import src.problemgenerator.filters as filters
-import src.problemgenerator.copy as copy
 
 data = np.array(["hello world",
                  "all your Bayes' theorems are belong to us"])
 
-x_node = array.Array(data.shape)
-x_node.addfilter(filters.Uppercase(.45))
-root_node = copy.Copy(x_node)
-out = root_node.process(data, np.random.RandomState(seed=42))
+root_node = array.Array(data.shape)
+root_node.addfilter(filters.Uppercase("prob"))
+out = root_node.generate_error(data, {'prob': .45})
 print(out)
 print("output shape:", out.shape, ", output dtype:", out.dtype)

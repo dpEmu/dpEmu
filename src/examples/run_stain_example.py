7f043b65d0a1b49f37d634f655353ad3dffdbb46
Juha Harviainen <juha.harviainen@gmail.com>
2019-07-15 16:01:06 +0300

    Fix run_stain_example

--- src/examples/run_stain_example.py
+++ src/examples/run_stain_example.py
@@ -20,10 +20,10 @@ def img_to_pixel_data(img):
 def main():
     img = Image.open("demo/landscape.png")
     data = img_to_pixel_data(img)
-    x_node = array.Array(data.shape)
-    x_node.addfilter(filters.StainArea(.00002, radius_generators.GaussianRadiusGenerator(50, 20), 0.9))
-    root_node = copy.Copy(x_node)
-    result = root_node.process(data, np.random.RandomState(seed=42))
+    root_node = array.Array(data.shape)
+    root_node.addfilter(filters.StainArea("p", "radius_gen", "alpha"))
+    params = {'p': .00002, "radius_gen": radius_generators.GaussianRadiusGenerator(50, 20), 'alpha': .9}
+    result = root_node.generate_error(data, params)
     filtered_img = Image.fromarray(result.astype('uint8'), 'RGB')
     filtered_img.show()
 

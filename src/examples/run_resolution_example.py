02811a56d639cff058bb2de035b57f57dd00964b
Juuso Silvennoinen <32332926+Jsos17@users.noreply.github.com>
2019-07-15 15:16:38 +0300

    Update examples

--- src/examples/run_resolution_example.py
+++ src/examples/run_resolution_example.py
@@ -5,7 +5,6 @@ import time
 import matplotlib.pyplot as plt
 import src.problemgenerator.array as array
 import src.problemgenerator.filters as filters
-import src.problemgenerator.copy as copy
 
 
 def main():
@@ -19,23 +18,21 @@ def main():
     data1 = plt.imread("demo/landscape.png")
     x_node1 = array.Array(data1.shape)
     r1 = filters.Resolution("resolution")
-    r1.set_params(d)
     x_node1.addfilter(r1)
-    root_node1 = copy.Copy(x_node1)
     start1 = time.time()
-    result1 = root_node1.process(data1, np.random.RandomState(seed=42))
+    result1 = x_node1.generate_error(data1, d)
     end1 = time.time()
     print(f"Time traditional: {end1-start1}")
 
     # Vectorized
+    # img2 = Image.open("demo/landscape.png")
+    # data2 = np.array(img2)
     data2 = plt.imread("demo/landscape.png")
     x_node2 = array.Array(data2.shape)
     r2 = filters.ResolutionVectorized("resolution")
-    r2.set_params(d)
     x_node2.addfilter(r2)
-    root_node2 = copy.Copy(x_node2)
     start2 = time.time()
-    result2 = root_node2.process(data2, np.random.RandomState(seed=42))
+    result2 = x_node2.generate_error(data2, d)
     end2 = time.time()
     print(f"Time vectorized: {end2-start2}")
 

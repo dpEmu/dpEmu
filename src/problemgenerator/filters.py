122bde82d517685994af7288dff3f409c80c4f17
Juha Mylläri <jmyllari@gmail.com>
2019-07-18 15:47:50 +0300

    Create audio clipping example

--- src/problemgenerator/filters.py
+++ src/problemgenerator/filters.py
@@ -843,6 +843,29 @@ class LensFlare(Filter):
             steps -= 1
 
 
+class ClipWAV(Filter):
+    def __init__(self, dyn_range_id):
+        super().__init__()
+        self.dyn_range_id = dyn_range_id
+
+    def set_params(self, params_dict):
+        self.dyn_range = params_dict[self.dyn_range_id]
+
+    def apply(self, node_data, random_state, named_dims):
+
+        def clip_audio(data, dyn_range):
+            min_, max_ = min(data), max(data)
+            half_range = .5 * max_ - .5 * min_
+            middle = (min_ + max_) / 2
+            new_half_range = half_range * dyn_range
+            upper_limit = middle + new_half_range
+            lower_limit = middle - new_half_range
+            return np.clip(data, lower_limit, upper_limit).astype(data.dtype)
+
+        for index, audio_data in np.ndenumerate(node_data):
+            node_data[index] = clip_audio(audio_data, self.dyn_range)
+
+
 class ApplyWithProbability(Filter):
     def __init__(self, ftr_id, probability_id):
         super().__init__()

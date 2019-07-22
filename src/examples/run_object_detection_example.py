fc7b718c1aa4da2cb9f654bce827c482a854bcfc
thalvari <tuomas.halvari@helsinki.fi>
2019-07-22 06:30:29 +0300

    change models

--- src/examples/run_object_detection_example.py
+++ src/examples/run_object_detection_example.py
@@ -16,12 +16,20 @@ def main():
     workspace.GlobalInit(["caffe2", "--caffe2_log_level=0"])
     setup_logging(__name__)
 
-    merge_cfg_from_file("venv/src/detectron/configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml")
+    # merge_cfg_from_file("venv/src/detectron/configs/12_2017_baselines/e2e_faster_rcnn_X-101-64x4d-FPN_1x.yaml")
+    # url_to_weights = (
+    #     "https://dl.fbaipublicfiles.com/detectron/35858015/12_2017_baselines/"
+    #     "e2e_faster_rcnn_X-101-64x4d-FPN_1x.yaml.01_40_54.1xc565DE/output/train/"
+    #     "coco_2014_train%3Acoco_2014_valminusminival/generalized_rcnn/model_final.pkl"
+    # )
+
+    merge_cfg_from_file("venv/src/detectron/configs/12_2017_baselines/e2e_mask_rcnn_X-101-64x4d-FPN_1x.yaml")
     url_to_weights = (
-        "https://dl.fbaipublicfiles.com/detectron/35861858/12_2017_baselines/"
-        "e2e_mask_rcnn_R-101-FPN_2x.yaml.02_32_51.SgT4y1cO/output/train/coco_2014_train:coco_2014_valminusminival/"
-        "generalized_rcnn/model_final.pkl"
+        "https://dl.fbaipublicfiles.com/detectron/36494496/12_2017_baselines/"
+        "e2e_mask_rcnn_X-101-64x4d-FPN_1x.yaml.07_50_11.fkwVtEvg/output/train/"
+        "coco_2014_train%3Acoco_2014_valminusminival/generalized_rcnn/model_final.pkl"
     )
+
     opt_list = [
         "MODEL.KEYPOINTS_ON",
         False,

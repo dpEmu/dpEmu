5c9c343f59e1d51c59aa4f4a723ad1000cff0605
Juha Mylläri <jmyllari@gmail.com>
2019-07-25 14:25:44 +0300

    Introduce LeafNode class

--- src/problemgenerator/array.py
+++ src/problemgenerator/array.py
@@ -1,29 +1,8 @@
 import numpy as np
-from src.problemgenerator.node import Node, get_node_data
+from src.problemgenerator.node import LeafNode, get_node_data, assign
 
 
-def assign(data_root, index_tuple, value):
-    """Makes the assignment data_root[index_tuple] = value.
-    """
-    if not index_tuple:
-        if len(value) == len(data_root):
-            data_root[:] = value[:]
-            return
-        else:
-            raise Exception(f"""Cannot assign value
-                                to data root
-                                """)
-    location = data_root
-    while len(index_tuple) > 1 and type(location) is not np.ndarray:
-        location = location[index_tuple[0]]
-        index_tuple = index_tuple[1:]
-    if len(index_tuple) == 1:
-        location[index_tuple[0]] = value
-    else:
-        location[index_tuple] = value
-
-
-class Array(Node):
+class Array(LeafNode):
     """An Array node represents a data array of any dimension (>= 0).
     One or more filters (error sources) can be added to the node.
     The filters are applied in the order in which they are added.
@@ -39,12 +18,15 @@ class Array(Node):
 
     def process(self, data, random_state, index_tuple=(), named_dims={}):
         """Apply all filters in this node."""
-        node_data, is_list, is_scalar = get_node_data(data, index_tuple)
+        node_data, is_list, is_scalar, is_tuple = get_node_data(data, index_tuple)
         if is_list:
             self.apply_filters(node_data, random_state, named_dims)
             assign(data, index_tuple, list(node_data))
         elif is_scalar:
             self.apply_filters(node_data, random_state, named_dims)
             assign(data, index_tuple, node_data[()])
+        elif is_tuple:
+            self.apply_filters(node_data, random_state, named_dims)
+            assign(data, index_tuple, tuple(node_data))
         else:
             self.apply_filters(node_data, random_state, named_dims)
--- src/problemgenerator/node.py
+++ src/problemgenerator/node.py
@@ -36,12 +36,47 @@ class Node:
         return copy_data
 
 
+class LeafNode(Node):
+    """LeafNode is the superclass for all leaf node classes of the error generation tree.
+    """
+
+    def __init__(self):
+        super().__init__([])
+
+    def apply_filters(self, node_data, random_state, named_dims):
+        for f in self.filters:
+            f.apply(node_data, random_state, named_dims)
+
+
 def get_node_data(data, index_tuple, make_array=True):
     index_list = list(index_tuple)
     while index_list:
         data = data[index_list.pop(0)]
     node_data_is_list = type(data) is list
     node_data_is_scalar = np.isscalar(data)
+    node_data_is_tuple = type(data) is tuple
     if make_array and type(data) is not np.ndarray:
         data = np.array(data)
-    return data, node_data_is_list, node_data_is_scalar
+    return data, node_data_is_list, node_data_is_scalar, node_data_is_tuple
+
+
+def assign(data_root, index_tuple, value):
+    """Makes the assignment data_root[index_tuple] = value.
+    """
+    if not index_tuple:
+        if len(value) == len(data_root):
+            data_root[:] = value[:]
+            return
+        else:
+            raise Exception(f"""Cannot assign value
+                                to data root
+                                """)
+    location = data_root
+    while len(index_tuple) > 1 and type(location) is not np.ndarray:
+        location = location[index_tuple[0]]
+        index_tuple = index_tuple[1:]
+    if len(index_tuple) == 1:
+        location[index_tuple[0]] = value
+    else:
+        location[index_tuple] = value
+

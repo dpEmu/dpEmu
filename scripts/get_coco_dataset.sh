729136e3951cd81edd21d5e36695fb8160a360e7
thalvari <tuomas.halvari@helsinki.fi>
2019-07-26 01:09:57 +0300

    small fix

--- scripts/get_coco_dataset.sh
+++ scripts/get_coco_dataset.sh
@@ -13,4 +13,5 @@ rm val2017.zip
 
 cd ..
 mkdir tmp/val2017
-java -jar data/cocotoyolo.jar "data/annotations/instances_val2017.json" "$PWD/tmp/val2017/" "all" "data/labels/"
+mkdir data/labels
+java -jar data/cocotoyolo.jar "data/annotations/instances_val2017.json" "$PWD/tmp/images/val2017/" "all" "tmp/labels/val2017/"

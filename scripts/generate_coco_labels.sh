d30e89d88e63ff7f400a3f546d53073ae0ccac0a
thalvari <tuomas.halvari@helsinki.fi>
2019-07-26 04:21:42 +0300

    clean repo

--- scripts/generate_coco_labels.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p tmp/images/val2017
-mkdir -p tmp/labels/val2017
-java -jar data/cocotoyolo.jar "data/annotations/instances_val2017.json" "$PWD/tmp/images/val2017/" "all" "tmp/labels/val2017/"

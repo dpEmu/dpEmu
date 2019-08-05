9db77ea3f99a20e46588c9748e40f30c00938252
Juha Mylläri <jmyllari@gmail.com>
2019-08-05 14:06:43 +0300

    Fix run_compression

--- examples/run_compression_example.py
+++ examples/run_compression_example.py
@@ -2,12 +2,12 @@ import numpy as np
 
 from PIL import Image
 
-import src.problemgenerator.array as array
-import src.problemgenerator.filters as filters
+from dpemu import array
+from dpemu import filters
 
 
 def main():
-    img = Image.open("demo/landscape.png")
+    img = Image.open("../demo/landscape.png")
     data = np.array(img)
     root_node = array.Array()
     root_node.addfilter(filters.JPEG_Compression('quality'))

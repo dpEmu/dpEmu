9c00e74eb6e2919c516216326f81ca2a78eb2184
thalvari <tuomas.halvari@helsinki.fi>
2019-08-06 13:47:46 +0300

    update weights

--- scripts/get_yolov3.sh
+++ scripts/get_yolov3.sh
@@ -3,4 +3,4 @@
 cd tmp
 
 wget -c https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov3-spp.cfg
-wget -c https://www.dropbox.com/s/jxr17c1bt6ih9oy/yolov3-spp_best.weights
+wget -c https://www.dropbox.com/s/qqo35n72pywv9af/yolov3-spp_best.weights

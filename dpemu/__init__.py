6207705b20f637690257711bd54d35c7e400b474
Juha Mylläri <jmyllari@gmail.com>
2019-08-08 13:56:38 +0300

    Move problemgenerator utils to dpemu

--- dpemu/__init__.py
+++ dpemu/__init__.py
@@ -1,6 +1,5 @@
 from . import utils
 from .plotting import utils as plotting_utils
-from .problemgenerator import utils as pg_utils
 from .ml import utils as ml_utils
 
 __all__ = ['array', 'series', 'tuple', 'filters', 'radius_generators',

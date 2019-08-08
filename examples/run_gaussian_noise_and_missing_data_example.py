54698384ea0561a1eca6a1856d5b7cffa78930ee
Juha Mylläri <jmyllari@gmail.com>
2019-08-08 14:29:18 +0300

    Fix imports in example

--- examples/run_gaussian_noise_and_missing_data_example.py
+++ examples/run_gaussian_noise_and_missing_data_example.py
@@ -2,9 +2,9 @@ import sys
 from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
-from dpemu import array
-from dpemu import filters
-from dpemu import series
+from dpemu.nodes import Array
+from dpemu.nodes import TupleSeries
+from dpemu.problemgenerator import filters
 
 std = float(sys.argv[1])
 prob = float(sys.argv[2])
@@ -15,11 +15,11 @@ y_file = Path("data/mnist_label.npy")
 x = np.load(x_file)[:n].reshape((n, 28, 28))
 y = np.load(y_file)[:n]
 
-x_node = array.Array()
+x_node = Array()
 x_node.addfilter(filters.GaussianNoise("mean", "std"))
 x_node.addfilter(filters.Missing("prob"))
-y_node = array.Array()
-root_node = series.TupleSeries([x_node, y_node])
+y_node = Array()
+root_node = TupleSeries([x_node, y_node])
 error_params = {"mean": 0, "std": std, "prob": prob}
 out_x, out_y = root_node.generate_error((x, y), error_params)
 

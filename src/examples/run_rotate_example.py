5e6317802ab867745f7ef7d4d12214ab50cf9217
Juuso Silvennoinen <32332926+Jsos17@users.noreply.github.com>
2019-08-08 11:51:45 +0300

    Add notebook support by nbsphinx

--- src/examples/run_rotate_example.py
+++ src/examples/run_rotate_example.py
@@ -1,6 +1,6 @@
 import sys
 import cv2
-# from PIL import Image
+import matplotlib.pyplot as plt
 
 
 import src.problemgenerator.array as array
@@ -17,8 +17,8 @@ def main():
     result = root_node.generate_error(data, {'angle': angle})
     # filtered_img = Image.fromarray(result)
     # filtered_img.show()
-    cv2.imshow("Rotated", result)
-    cv2.waitKey(0)
+    plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+    plt.show()
 
 
 if __name__ == "__main__":

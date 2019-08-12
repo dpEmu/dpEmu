756f22d2d996f87083a225773bdd37ccdbc50337
thalvari <tuomas.halvari@helsinki.fi>
2019-08-12 11:57:00 +0300

    modify reqs

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'tutorials/.ipynb_checkpoints']
 
+pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 

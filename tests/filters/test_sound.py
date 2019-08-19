94fa992155a8aaddd83bbb1a269e1bd381438316
Juuso Silvennoinen <32332926+Jsos17@users.noreply.github.com>
2019-08-19 14:44:53 +0300

    Add tests for sound.py

---
+++ tests/filters/test_sound.py
@@ -0,0 +1,20 @@
+import numpy as np
+from dpemu.filters.sound import ClipWAV
+
+
+def test_clip_wav():
+    dyn_ranges = [0.0, 0.22, 0.5, 0.73, 1.0]
+    for dyn_range in dyn_ranges:
+        rng = np.random.RandomState(1729)
+
+        data = rng.rand(100) * 100
+        min_, max_ = np.amin(data), np.amax(data)
+        new_half_range = 0.5 * (max_ - min_) * dyn_range
+        mid = (max_ + min_) / 2
+        new_data = np.clip(data, mid - new_half_range, mid + new_half_range)
+
+        clip_wav = ClipWAV("dyn_range")
+        clip_wav.set_params({"dyn_range": dyn_range})
+        clip_wav.apply(data, rng, named_dims={})
+
+        assert np.allclose(new_data, data)

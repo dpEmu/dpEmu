3af5e859b121a0ce9f70a0422679fcaa597cc2f6
Juha Myllari <jmyllari@gmail.com>
2019-08-25 21:12:53 +0300

    Fix reshape in _load_mnist

--- dpemu/dataset_utils.py
+++ dpemu/dataset_utils.py
@@ -88,9 +88,9 @@ def load_mnist(n_data=70000):
 
 def _load_mnist(reshape_to_28x28=False):
     (x_train, y_train), (x_test, y_test) = load_mnist_data()
-    if reshape_to_28x28:
-        x_train = x_train.reshape((-1, 28, 28))
-        x_test = x_test.reshape((-1, 28, 28))
+    if not reshape_to_28x28:
+        x_train = x_train.reshape((-1, 28*28))
+        x_test = x_test.reshape((-1, 28*28))
     return x_train, y_train, x_test, y_test
 
 def load_fashion(n_data=70000):

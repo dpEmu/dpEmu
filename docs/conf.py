3701b254519a7edfab2e51ba2ff2071b6f33c01c
Juuso Silvennoinen <32332926+Jsos17@users.noreply.github.com>
2019-08-29 14:24:09 +0300

    Add official version/release number

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2019, Tuomas Halvari, Juha Harviainen, Juha Mylläri, Antti Röysk�
 author = 'dpEmu Team'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
